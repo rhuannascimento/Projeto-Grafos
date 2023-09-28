765c500daa111a4cdd43fc11fe711ab918646f9a
igor <guedes.igor018@gmail.com>
2023-09-27 21:43:15 -0300

    feat: ler grao do arquivo

--- main.cpp
+++ main.cpp
@@ -5,38 +5,44 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    cout <<endl << "--------------------------------------------------------------------------------------------------" << endl<< endl;
+int main(int argc, char *argv[])
+{
+    cout << endl << "--------------------------------------------------------------------------------------------------" << endl << endl;
 
-    Grafo * g = new Grafo();
+    Grafo *g = new Grafo();
 
+   
+    ifstream arquivo("instancias_grafos_n_ponderados/ex1.txt"); // Abre o arquivo para leitura
+    
 
-
-    g->insereNo(2);
-    g->insereNo(4);
-    g->insereNo(5);
-    g->insereNo(6);
-    g->insereNo(7);
-
-    g->insereAresta(4, 2);
-    g->insereAresta(4, 5);
-    g->insereAresta(2, 4);
-
-    g->imprimeGrafo();
-
-
-    /*string archive_name = argv[1];
-    ifstream archive("../instancias_grafos_n_ponderados/" + archive_name);
-    if (!archive.is_open()) {
-        cerr << "Erro ao abrir o arquivo " << archive_name << endl;
+    if (!arquivo.is_open())
+    {
+        cout << "Erro ao abrir o arquivo." << std::endl;
         return 1;
     }
-    string line;    
-    while (getline(archive, line)) {
-        cout<< line << endl;
+
+    int nodeNumbers;
+
+    if (arquivo >> nodeNumbers)
+    {
+        for (int i = 1; i < nodeNumbers; i++)
+        {
+            g->insereNo(i);
+        }
+    }
+
+    
+
+    int a, b;
+    while (arquivo >> a >> b)
+    {
+        g->insereAresta(a, b);
+        g->insereAresta(b, a);
         break;
     }
-    archive.close();*/
+    g->imprimeGrafo();
+
+    arquivo.close(); // Fecha o arquivo
 
     return 0;
 }
