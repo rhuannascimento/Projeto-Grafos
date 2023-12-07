4c30936110306d4066127313d14c143d6366a500
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2023-12-06 22:02:46 -0300

    Adiciona instancias

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf build && mkdir build && g++ -std=c++17 -g main.cpp ./src/*.cpp -o ./build/main && ./build/main instancias_grafo_ponderados/ex1.txt output_file.txt 1 1 X
+rm -rf build && mkdir build && g++ -std=c++17 -g main.cpp ./src/*.cpp -o ./build/main && ./build/main instancias_grafo_ponderados/100.txt output_file.txt 1 1 0
